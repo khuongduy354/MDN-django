2aff4ddc73c3eed2a4ed0f95e302685a6706bcdf
khuongduy354 <khuongduy354@gmail.com>
2021-08-24 18:07:38 +0700

    add list and detail of books

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from catalog.models import BookInstance
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
@@ -28,3 +29,17 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model=Book
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+class BookDetailView(generic.DetailView):
+    model=Book
