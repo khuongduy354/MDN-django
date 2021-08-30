2cccbea1ef5c409ec74f792f9868cf429dfb2096
khuongduy354 <khuongduy354@gmail.com>
2021-08-30 21:23:34 +0700

    last commit

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,7 +7,7 @@ admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Language)
 
-@admin.register(BookInstance)
+@admin.register(BookInstance)       
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
--- locallib/urls.py
+++ locallib/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
 #Add URL maps to redirect the base URL to our application
 from django.views.generic import RedirectView
 urlpatterns += [
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('', RedirectView.as_view(url='catalog/', permanent=False)),
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
@@ -34,6 +34,7 @@ from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
 
+#login 
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
